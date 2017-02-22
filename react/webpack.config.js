ccb324c0ea1fc3f39d3d04ddf3888d3fba2639a6
twcn <twcn@twcns-MacBook-Pro-2.local>
2017-02-23 00:28:15 +0800

    react

---
+++ react/webpack.config.js
@@ -0,0 +1,29 @@
+ var path = require('path');
+ var webpack = require('webpack');
+     
+ module.exports = {
+     entry: './js/app.js',
+     resolve: {
+         extensions: [ '.js', '.jsx'],
+     },
+     output: {
+         path: path.resolve(__dirname, 'build'),
+         filename: 'app.bundle.js'
+     },
+     module: {
+         loaders: [
+             {
+                 test: /\.js$/,
+                 loader: 'babel-loader',
+                 query: {
+                     presets: ['es2015', 'react']
+                 }
+             }
+         ]
+     },
+     stats: {
+         colors: true
+     },
+     devtool: 'source-map'
+ };
+
